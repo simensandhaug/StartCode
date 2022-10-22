e50313fb8752ff922818cc0a7e3c4d84c864a5c9
Oskar Jorgensen <oskarjorgensen@gmail.com>
2022-10-22 16:45:28 +0200

    update to use new parseDate function

--- parsers/gyroscopeSensorParser.py
+++ parsers/gyroscopeSensorParser.py
@@ -12,9 +12,6 @@ class GyroscopeSensorParser(SensorParser):
         isdpt, depth, _, pressure, _ = line[0].split(",")
         return (isdpt, float(depth), float(pressure))
     
-    def parse_start_date(self, sample_start):
-        return sample_start.strip()[21:]
-    
     def parseData(self, file, metadata):
         # Convert lines to a list like this:
         '''
@@ -37,8 +34,7 @@ class GyroscopeSensorParser(SensorParser):
         i = 0
         measurementPairs = []
         while i < total_lines-1:
-            sample_start = self.parse_start_date(data[i])
-            sample_start_parsed = self.parseDate(sample_start)
+            sample_start_parsed = self.parseDate(data[i])
             line = ast.literal_eval(data[i+1])
             line_len = len(line)
             # float_measurements = data_lines[i:i+line_len]
